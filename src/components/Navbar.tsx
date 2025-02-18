b501296d8142f25f95bc0c27d28b591f1ff598f2
anuxoxo <anushka.ak14@gmail.com>
2025-02-18 23:35:08 +0530

    navbar responsive done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 import { Link, useLocation } from "react-router-dom";
-
+import { BiSolidShip } from "react-icons/bi";
 import styles from "../styles/Navbar.module.scss";
 
 const Navbar = memo(() => {
@@ -8,22 +8,28 @@ const Navbar = memo(() => {
 
   return (
     <div className={`${styles.Navbar}`}>
-      <Link
-        className={`${styles.Navbar__item} ${
-          path.pathname === "/" && styles.Navbar__item_active
-        }`}
-        to="/"
-      >
-        Form
-      </Link>
-      <Link
-        className={`${styles.Navbar__item} ${
-          path.pathname === "/table" && styles.Navbar__item_active
-        }`}
-        to="/table"
-      >
-        Table
-      </Link>
+      <div className={`${styles.Navbar__icon}`}>
+        <BiSolidShip size={40} color="#7743db" />
+        <p>ShipBox</p>
+      </div>
+      <div className={`${styles.Navbar__links}`}>
+        <Link
+          className={`${styles.Navbar__item} ${
+            path.pathname === "/" && styles.Navbar__item_active
+          }`}
+          to="/"
+        >
+          Add Box
+        </Link>
+        <Link
+          className={`${styles.Navbar__item} ${
+            path.pathname === "/table" && styles.Navbar__item_active
+          }`}
+          to="/table"
+        >
+          View Boxes
+        </Link>
+      </div>
     </div>
   );
 });
--- src/pages/AddBox.tsx
+++ src/pages/AddBox.tsx
@@ -1,8 +1,13 @@
 import { lazy } from "react";
 const BoxForm = lazy(() => import("../components/BoxForm"));
+import styles from "../styles/AddBox.module.scss";
 
 const AddBox = () => {
-  return <BoxForm />;
+  return (
+    <div className={styles.AddBox}>
+      <BoxForm />
+    </div>
+  );
 };
 
 export default AddBox;
